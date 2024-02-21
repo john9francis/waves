5acd11ea6c98aee92463beae50722e6c7593a3e2
John Francis <john9francis@gmail.com>
2024-02-21 14:52:53 -0700

    Moved the starter code to it's own file

---
+++ test.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import time
+
+def clear_console():
+  os.system('cls' if os.name == 'nt' else 'clear')
+
+def simple_wave():
+  # Let's get a simple wave going
+
+  # start our wave arrays for time -1, 0 and 1
+  wave_time_array1 = np.array([0 for _ in range(10)])
+  wave_time_array2 = np.array([0 for _ in range(10)])
+  wave_time_array3 = np.array([0 for _ in range(10)])
+
+  # set initial conditions
+  wave_time_array1[3] = 1
+  wave_time_array2[4] = 1
+  wave_time_array3[5] = 1
+
+  # create a loop to do a simple animation
+  l = [wave_time_array1, wave_time_array2, wave_time_array3]
+  for _ in range(10):
+    for i in l:
+      print(i)
+      time.sleep(.2)
+      clear_console()
+
+
+
+if __name__ == "__main__":
+  simple_wave()
